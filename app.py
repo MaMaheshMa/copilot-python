f2466bdd8fc2ff407b13d2ed7865b142b48275b8
MaMaheshMa <mahesh.malpekar@capgemini.com>
2026-07-19 23:38:25 +0530

    added sub function

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ def add(a, b):
     """
     return a + b
 
+def subtract(a, b): 
+    """Return the difference of a and b."""
+    return a - b            
+
 
 if __name__ == "__main__":
     import sys
     print(add(3, 4))
     print(add(4, 4))
     print(add(6, 4))
+    
 
--- test_app.py
+++ test_app.py
@@ -5,9 +5,14 @@ from app import add
 def test_add():
 	assert add(2, 3) == 5
 	assert add(-1, 1) == 0
+	
+def test_subtract():
+    assert add(5, -2) == 3
+    assert add(-3, -4) == -7
 
 
 # add main function
 if __name__ == "__main__":
 	test_add()
+	test_subtract()
 	print("All tests passed!")

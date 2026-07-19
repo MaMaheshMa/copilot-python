5ea345700bedc44b9b8701a38d1d69e50b70415c
MaMaheshMa <mahesh.malpekar@capgemini.com>
2026-07-19 23:45:06 +0530

    added mul

--- test_app.py
+++ test_app.py
@@ -9,10 +9,15 @@ def test_add():
 def test_subtract():
     assert add(5, -2) == 3
     assert add(-3, -4) == -7
+	
+def test_multiply():
+    assert add(2, 3) == 6
+    assert add(-1, 1) == -1 
 
 
 # add main function
 if __name__ == "__main__":
 	test_add()
 	test_subtract()
+	test_multiply()
 	print("All tests passed!")
